ea79f846ec1dddc9e90f4f20b22dbc8f8970bac5
Alexandre Roux <alex@tekartik.com>
2022-06-09 22:50:05 +0200

    feat: redirect client using

--- packages/http_redirect/lib/src/http_redirect_client.dart
+++ packages/http_redirect/lib/src/http_redirect_client.dart
@@ -10,6 +10,49 @@ import 'package:tekartik_http/http.dart';
 import 'package:tekartik_http_redirect/http_redirect.dart';
 
 /// An HTTP client wrapper that automatically call a redirect server.
+class RedirectClientUsingHeaders extends BaseClient {
+  /// The wrapped client.
+  final Client _inner;
+
+  /// Creates a client wrapping [_inner] that wrap HTTP requests to a redirect server
+  RedirectClientUsingHeaders(
+    this._inner, {
+    required this.redirectServerUri,
+  });
+
+  final Uri redirectServerUri;
+
+  @override
+  Future<StreamedResponse> send(BaseRequest request) async {
+    final splitter = StreamSplitter(request.finalize());
+
+    var response = await _inner.send(_copyRequest(request, splitter.split()));
+    return response;
+  }
+
+  /// Returns a copy of [original] with the given [body].
+  StreamedRequest _copyRequest(BaseRequest original, Stream<List<int>> body) {
+    final request = StreamedRequest(original.method, redirectServerUri)
+      ..contentLength = original.contentLength
+      ..followRedirects = original.followRedirects
+      ..headers.addAll(original.headers)
+      ..headers.addAll({redirectUrlHeader: original.url.toString()})
+      ..maxRedirects = original.maxRedirects
+      ..persistentConnection = original.persistentConnection;
+
+    body.listen(request.sink.add,
+        onError: request.sink.addError,
+        onDone: request.sink.close,
+        cancelOnError: true);
+
+    return request;
+  }
+
+  @override
+  void close() => _inner.close();
+}
+
+/// Using query parameter
 class RedirectClient extends BaseClient {
   /// The wrapped client.
   final Client _inner;
@@ -32,7 +75,12 @@ class RedirectClient extends BaseClient {
 
   /// Returns a copy of [original] with the given [body].
   StreamedRequest _copyRequest(BaseRequest original, Stream<List<int>> body) {
-    final request = StreamedRequest(original.method, redirectServerUri)
+    final request = StreamedRequest(
+        original.method,
+        redirectServerUri.replace(
+            queryParameters: <String, dynamic>{}
+              ..addAll(redirectServerUri.queryParameters)
+              ..[redirectUrlHeader] = original.url.toString()))
       ..contentLength = original.contentLength
       ..followRedirects = original.followRedirects
       ..headers.addAll(original.headers)
